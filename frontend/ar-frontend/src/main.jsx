21a260dcb9b71dcfba13e80f6e57350d2896a6c0
pankajUE203080 <sdev5281@gmail.com>
2025-12-25 17:42:47 +0530

    new changes

--- frontend/ar-frontend/src/main.jsx
+++ frontend/ar-frontend/src/main.jsx
@@ -2,7 +2,8 @@ import '@google/model-viewer';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './app.css';
+import './App.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
